e3550a6917a32b2e2d99e9594ff514c14aa6c63d
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-08-18 19:13:57 +0700

    feat: add file structure

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname})
+})
+
 app.listen(PORT, () => {
     console.log(`Server listen on port ${PORT}`)
 })
