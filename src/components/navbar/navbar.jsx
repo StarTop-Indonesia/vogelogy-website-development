1fb77eddf2888364392ee119a398712a84a8424b
mkhoiqi <mkhoirurrizqi@gmail.com>
2021-08-29 19:58:25 +0700

    feat: add navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src="/asset/other_img/Logo_vogelogy.png" alt="Vogelogy Logo" /> Vogelogy
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#search">
+                Search
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#community">
+                Community
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
