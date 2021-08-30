d66b95c231190f648363afc308b60dec8dbc452c
mkhoiqi <mkhoirurrizqi@gmail.com>
2021-08-30 21:51:32 +0700

    feat: update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import "./navbar.css";
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
+    <nav className="navbar navbar-expand-lg navbar-light ps-5 pe-5">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
-          <img src="/asset/other_img/Logo_vogelogy.png" alt="Vogelogy Logo" /> Vogelogy
+          <img src="/asset/other_img/Logo_vogelogy.png" alt="Vogelogy Logo" className="me-4" />
+          <span>Vogelogy</span>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item me-5">
               <a className="nav-link" href="#search">
                 Search
               </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item me-5">
               <a className="nav-link" href="#about">
                 About
               </a>
