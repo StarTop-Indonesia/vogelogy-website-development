f6260f10eae5e7c47eac1d4bb6ac5b6fcf8babe1
mkhoiqi <mkhoirurrizqi@gmail.com>
2021-09-15 00:17:02 +0700

    feat: rebuild searchbar css for mobile view

--- src/components/search_page/searchBar.jsx
+++ src/components/search_page/searchBar.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import "./searchBar.css";
 const searchBar = () => {
   return (
-    <div className="search-wrapper d-flex row ps-3 pe-3">
+    <div className="search-wrapper d-flex row">
       <div className="search-column col-9">
         <form action="" className="col-12">
           <input type="text" name="" id="" placeholder="Search with bird name ..." />
         </form>
       </div>
-      <div className="icon-wrapper justify-content-end col-3">
-        <a href="" className="camera-icon me-4">
+      <div className="icon-wrapper justify-content-end col-3 justify-content-around">
+        <a href="#" className="camera-icon">
           <img src="/asset/other_img/camera-icon.png" alt="Camera Icon" />
         </a>
-        <a href="" className="search-icon me-4">
+        <a href="#" className="search-icon">
           <img src="/asset/other_img/search-icon.png" alt="Search Icon" />
         </a>
       </div>
